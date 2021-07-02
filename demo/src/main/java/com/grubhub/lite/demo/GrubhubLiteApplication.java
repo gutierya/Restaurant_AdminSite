0f59e31fa9bf7fd6b47fda3bc22786188c3f072a
Hector Ramirez <github.hector1@gmail.com>
2021-07-02 11:18:18 -0400

    dev create

---
+++ demo/src/main/java/com/grubhub/lite/demo/GrubhubLiteApplication.java
@@ -0,0 +1,13 @@
+package com.grubhub.lite.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrubhubLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrubhubLiteApplication.class, args);
+	}
+
+}
