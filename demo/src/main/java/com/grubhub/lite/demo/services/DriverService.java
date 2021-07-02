33842e29673553fac42ba82cabe32b4f9c7d90a7
jasminelin18 <71404828+jasminelin18@users.noreply.github.com>
2021-07-02 16:50:45 -0400

    adding DriverService

--- demo/src/main/java/com/grubhub/lite/demo/services/DriverService.java
+++ demo/src/main/java/com/grubhub/lite/demo/services/DriverService.java
@@ -3,7 +3,10 @@ package com.grubhub.lite.demo.services;
 import com.grubhub.lite.demo.exceptions.customer.CustomerAlreadyExistsException;
 import com.grubhub.lite.demo.exceptions.driver.DriverNotFoundException;
 import com.grubhub.lite.demo.models.Driver;
+import com.grubhub.lite.demo.models.Enums;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class DriverService {
@@ -22,5 +25,48 @@ public class DriverService {
         throw new DriverNotFoundException(id);
     }
 
+    public void setRating (Short newVar, long id) throws DriverNotFoundException {
+        if (RepositoryService.getDriverRepository().existsById(id)) {
+            RepositoryService.getDriverRepository().getById(id).setRating(newVar);
+        }
+        throw new DriverNotFoundException(id);
+    }
+
+    public Short getRating (long id) throws DriverNotFoundException {
+        if (RepositoryService.getDriverRepository().existsById(id)) {
+            return RepositoryService.getDriverRepository().getById(id).getRating();
+        }
+        throw new DriverNotFoundException(id);
+    }
+
+    public void setVehicle (Enums.VehicleType newVar, long id) throws DriverNotFoundException {
+        if (RepositoryService.getDriverRepository().existsById(id)) {
+            RepositoryService.getDriverRepository().getById(id).setVehicle(newVar);
+        }
+        throw new DriverNotFoundException(id);
+    }
+
+    public Enums.VehicleType getVehicle(long id) throws DriverNotFoundException {
+        if (RepositoryService.getDriverRepository().existsById(id)) {
+            return RepositoryService.getDriverRepository().getById(id).getVehicle();
+        }
+        throw new DriverNotFoundException(id);
+    }
+
+    public void setEstimatedArrivalTime (Date newVar, long id) throws DriverNotFoundException {
+        if (RepositoryService.getDriverRepository().existsById(id)) {
+            RepositoryService.getDriverRepository().getById(id).setEstimatedArrivalTime(newVar);
+        }
+        throw new DriverNotFoundException(id);
+
+    }
+
+    public Date getEstimatedArrivalTime(long id) throws DriverNotFoundException {
+        if (RepositoryService.getDriverRepository().existsById(id)) {
+            return RepositoryService.getDriverRepository().getById(id).getEstimatedArrivalTime();
+        }
+        throw new DriverNotFoundException(id);
+
+    }
 
 }
