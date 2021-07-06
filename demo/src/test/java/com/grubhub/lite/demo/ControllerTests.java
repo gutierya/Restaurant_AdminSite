2a035f391a686c5a3627e768d7bcdaf3c836d85e
Yamilet <52222018+gutierya@users.noreply.github.com>
2021-07-06 13:53:40 -0400

    added testing maven dependancies for controllers

--- demo/src/test/java/com/grubhub/lite/demo/ControllerTests.java
+++ demo/src/test/java/com/grubhub/lite/demo/ControllerTests.java
@@ -1,4 +1,34 @@
 package com.grubhub.lite.demo;
 
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * tests to test endpts functionality instead of the actual underlying
+ * service beneath it
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTests {
+
+
+    @Test
+    void contextLoads(){
+
+    }
+
+    @Test
+    public void testGetCustomers() {
+        //Arrange
+        final String baseUrl = "http://localhost:" + port + "/";
+
+
+        //Act
+
+        //Assert
+    }
+
+
+
+
 }
